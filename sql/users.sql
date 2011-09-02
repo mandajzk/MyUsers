88f76cd96849e478d10164cc92fce64365f2f6ce
Steve Fuller <steve@redhousewd.com>
2011-09-02 12:48:33 -0400

    added displayed name field to db table

--- sql/users.sql
+++ sql/users.sql
@@ -7,6 +7,7 @@ CREATE TABLE `users` (
   `user_name` varchar(16) NOT NULL,
   `user_pass` varchar(16) NOT NULL,
   `user_hint` varchar(80) NOT NULL,
+  `user_dname` varchar(16) NOT NULL,
   `user_date` datetime NOT NULL,
   `user_level` int(3) default NULL,
   PRIMARY KEY  (`user_id`)
@@ -15,5 +16,5 @@ CREATE TABLE `users` (
 -- ----------------------------
 -- Records of Users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', 'steve', 'letmein', 'let them in', '2011-08-18 21:43:27', '2');
-INSERT INTO `users` VALUES ('2', 'dave', 'bobcat', 'a type of critter', '2011-08-18 21:43:27', '2');
+INSERT INTO `users` VALUES ('1', 'steve', 'letmein', 'let them in', 'Steve', '2011-08-18 21:43:27', '2');
+INSERT INTO `users` VALUES ('2', 'dave', 'bobcat', 'a type of critter', 'David', '2011-08-18 21:43:27', '2');
