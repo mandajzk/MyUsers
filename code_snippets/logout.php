1331c62015c2086d939196b3091443594a67d1c2
Steve Fuller <steve@redhousewd.com>
2011-09-02 13:00:27 -0400

    added logout script

---
+++ code_snippets/logout.php
@@ -0,0 +1,16 @@
+﻿<?php
+/* Log Out Script */
+session_start();
+
+if (isset($_SESSION['logged_in'])) {
+   unset($_SESSION['logged_in']);
+   session_destroy;
+}
+
+/*  - Use this code to redirect your user to another page upon logout - 
+    - Could be a logged out message page or a login page if need be   - 
+
+header('Location: login.php');
+
+*/
+?>
